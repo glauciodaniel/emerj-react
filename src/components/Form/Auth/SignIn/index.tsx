0aad43903fdf0811be326c87e1177d4ff97dd32e
Glaucio Daniel <glauciodaniel@gmail.com>
2022-01-24 20:47:45 -0300

    Deploy

--- src/components/Form/Auth/SignIn/index.tsx
+++ src/components/Form/Auth/SignIn/index.tsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { Alert, Button } from '@mui/material';
+import { Alert, AlertColor, Button } from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
 import axios from 'axios';
 
@@ -25,7 +25,12 @@ const Container = styled.div`
 
         }
 `
-
+//export type AlertColor = 'success' | 'info' | 'warning' | 'error';
+interface Message {
+    severity: AlertColor;
+    text: string;
+    status: number;
+}
 
 export default function SignIn(){
 
@@ -33,6 +38,12 @@ const [formData, setFormData] = useState(defaultFormData);
 //destructuring Assignment
 const {email, password} = formData;
 const [success, setSuccess] =useState(false);
+const [error, setError] =useState<string | boolean>(false);
+const [message, setMessage] = useState<Message>({
+    severity:'error',
+    text:'',
+    status:403
+});
 const onChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     
     setFormData((prevState)=>({
@@ -42,16 +53,24 @@ const onChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
 }
 const onSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-   
     const response = await axios.post(
         'http://20.120.7.70/auth/login', formData
     );
-    
-    console.log(response.data);
-
     if(response.data.accessToken){
         localStorage.setItem("accessToken", response.data.accessToken);
         setSuccess(true);
+        setMessage({
+            severity: 'success',
+            text:'Usuário autenticado com sucesso!',
+            status:200
+        })
+    }else {
+        setError("Usuário inexistente ou senha inválida");
+        setMessage({
+            severity: 'error',
+            text:'Usuário inexistente ou senha inválida!',
+            status:403
+        })
     }
 }
     return (
@@ -86,6 +105,13 @@ const onSubmit = async (e: React.FormEvent) => {
                         type="submit"
                         >Enviar</Button> 
         {success &&  <Alert severity="success">Usuário autenticado com sucesso</Alert>}
+        {error &&  <Alert severity="error">{error}</Alert>}
+
+        {message.text ? (
+            <Alert severity={message.severity}>{message.text}</Alert>
+        ):('')}
+
+          
                 </Box>
             
         </Container>
